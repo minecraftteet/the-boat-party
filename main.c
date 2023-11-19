4c5acc1564018e86de0abb51cee39f87a87548b3
olivia <trevorleecox@icloud.com>
2023-11-18 22:46:10 -0600

    the best

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdint.h>
 
-int tunN;
+int tunN = 0;
 char playername[16];
 int opc;
 
@@ -13,13 +13,25 @@ int main()
   scanf("%s",playername);
   printf("\n");
   printf("hi %s\n", playername);
+  printf("you haied a scream on the top deck \n you feel the boat shake.\n");
   while (tunN != 100){
 
-    printf("what are you goig to do");
-    scanf("%d",opc);
-    opc =0;
-    tunN++;
+    printf("do you {1}run out the door. {2}look arond for stuff {3}just stand there");
+    scanf("%d",&opc);
+    tunN ++;
+    if(opc==1){
+      printf("%d\n",opc)
+    } else if (opc == 2) {
+      
+      
+    } else if (opc == 3) {
+      
+      
+    }
     
+
+
+    tunN++;
   }
 
   
