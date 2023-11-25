b817d2d3c0cc6efd23ce32583a450af098847972
olivia <trevorleecox@icloud.com>
2023-11-25 14:54:20 -0600

    a bunsh

--- headers/wined.h
+++ headers/wined.h
@@ -1,9 +1,13 @@
-int dead(){
+#include <cstdlib>
+#include <stdio.h>
+int dead() {
   printf("your dead\n");
   return 0;
 }
-int win()
-{
-  printf("\n");
-}
+int win() {
+  printf("you make it to the life bots at the last secoin.\nyou make it to the "
+         "main land where on the way of the life boad you slip and bust you "
+         "haed open on the gronde. killing you\n");
 
+  return 0;
+}
